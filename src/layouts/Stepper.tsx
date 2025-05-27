5c50e7ba456bd3924e8fc5d2e994553093d04dde
Nicha <nicha.wilanan@gmail.com>
2025-05-27 10:47:06 +0100

    feat: remove the second stepper

--- src/layouts/Stepper.tsx
+++ src/layouts/Stepper.tsx
@@ -6,8 +6,8 @@ import EmotionStep from "../pages/EmotionStep";
 const Stepper = () => {
   const mockSummaryText =
     "The data on social media paints a concerning picture. Relentless increases in daily usage steal time from real-world connections and personal reflection, potentially fueling loneliness. Engagement metrics highlight a troubling reliance on online validation, fostering insecurity through constant comparison with curated online personas. Rising FOMO statistics reveal a pervasive anxiety of missing out, driving compulsive platform checking. Furthermore, data on cyberbullying underscores the emotional toll of online negativity. These figures aren't just numbers; they signal a worrying trend of digital entanglement impacting our emotional well-being. We must address this growing concern to foster a healthier, more balanced relationship with social media before its allure overshadows genuine connection and peace of mind.";
-  type steps = "upload_file" | "choose_emotion" | "summary";
-  const [activeStep, setActiveStep] = useState<steps>("upload_file");
+  type steps = "first" | "second";
+  const [activeStep, setActiveStep] = useState<steps>("first");
 
   const [uploadedFile, setUploadedFile] = useState<File | null>(null);
   const [storyDescription, setStoryDescription] = useState<string>("");
@@ -26,24 +26,16 @@ const Stepper = () => {
     setSelectedEmotion(emotion);
   };
 
-  const nextStep = (currentStep: steps) => {
-    if (currentStep == "upload_file") {
-      return "choose_emotion";
-    }
-
-    return "summary";
+  const nextStep = () => {
+    return "second";
   };
 
-  const previousStep = (currentStep: steps) => {
-    if (currentStep == "summary") {
-      return "choose_emotion";
-    }
-
-    return "upload_file";
+  const previousStep = () => {
+    return "first";
   };
 
   const StepperBody = () => {
-    if (activeStep === "upload_file") {
+    if (activeStep === "first") {
       return (
         <UploadStep
           props={{
@@ -57,18 +49,6 @@ const Stepper = () => {
       );
     }
 
-    if (activeStep === "choose_emotion") {
-      return (
-        <EmotionStep
-          props={{
-            isLoading: isLoadingEmotion,
-            emotion: selectedEmotion,
-            emotionStepperEmitEmotion: handleEmotionChange,
-          }}
-        />
-      );
-    }
-
     return <SummaryStep props={{ summaryText: mockSummaryText }} />;
   };
 
@@ -77,7 +57,7 @@ const Stepper = () => {
       <ol className="flex items-center w-full justify-center p-3 space-x-2 text-sm font-medium text-center text-gray-500 bg-white  rounded-lg  dark:text-gray-400 sm:text-base dark:bg-gray-800 dark:border-gray-700 sm:p-4 sm:space-x-4 rtl:space-x-reverse">
         <li
           className={
-            activeStep === "upload_file"
+            activeStep === "first"
               ? "flex items-center text-blue-600"
               : "flex items-center text-gray-600"
           }
@@ -85,7 +65,7 @@ const Stepper = () => {
           <span className="flex items-center justify-center w-5 h-5 me-2 text-xs border border-gray-500 rounded-full shrink-0 dark:border-blue-500">
             1
           </span>
-          Upload file and Description
+          Upload file & Description
           <svg
             className="w-3 h-3 ms-2 sm:ms-4 rtl:rotate-180"
             aria-hidden="true"
@@ -102,9 +82,10 @@ const Stepper = () => {
             />
           </svg>
         </li>
+
         <li
           className={
-            activeStep === "choose_emotion"
+            activeStep === "second"
               ? "flex items-center text-blue-600"
               : "flex items-center text-gray-600"
           }
@@ -112,34 +93,7 @@ const Stepper = () => {
           <span className="flex items-center justify-center w-5 h-5 me-2 text-xs border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
             2
           </span>
-          Choose Emotion
-          <svg
-            className="w-3 h-3 ms-2 sm:ms-4 rtl:rotate-180"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 12 10"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="m7 9 4-4-4-4M1 9l4-4-4-4"
-            />
-          </svg>
-        </li>
-        <li
-          className={
-            activeStep === "summary"
-              ? "flex items-center text-blue-600"
-              : "flex items-center text-gray-600"
-          }
-        >
-          <span className="flex items-center justify-center w-5 h-5 me-2 text-xs border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
-            3
-          </span>
-          Summary
+          User Agency & Summary
         </li>
       </ol>
       <div className="grid my-16 mx-50 gap-y-10">
@@ -147,14 +101,14 @@ const Stepper = () => {
       </div>
       <div className="flex justify-center">
         <button
-          onClick={() => setActiveStep(previousStep(activeStep))}
+          onClick={() => setActiveStep(previousStep())}
           type="button"
           className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
         >
           Previous
         </button>
         <button
-          onClick={() => setActiveStep(nextStep(activeStep))}
+          onClick={() => setActiveStep(nextStep())}
           type="button"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
