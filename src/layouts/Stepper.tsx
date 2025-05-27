1379373c1100adb04a2db2b84c03ba195108113a
Nicha <nicha.wilanan@gmail.com>
2025-05-27 11:55:31 +0100

    style: modify stepper header and footer

--- src/layouts/Stepper.tsx
+++ src/layouts/Stepper.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import { FaAnglesLeft, FaAnglesRight } from "react-icons/fa6";
+
 import FirstPage from "../pages/FirstPage";
 import SecondPage from "../pages/SecondPage";
 
@@ -46,66 +48,70 @@ const Stepper = () => {
 
   return (
     <>
-      <ol className="flex items-center w-full justify-center p-3 space-x-2 text-sm font-medium text-center text-gray-500 bg-white  rounded-lg  dark:text-gray-400 sm:text-base dark:bg-gray-800 dark:border-gray-700 sm:p-4 sm:space-x-4 rtl:space-x-reverse">
-        <li
+      <ol className="flex items-center w-full justify-center p-3 space-x-2 text-sm font-medium text-center text-gray-500 sm:text-base sm:p-4 sm:space-x-4 rtl:space-x-reverse">
+        <button
+          type="button"
           className={
             activeStep === "first"
-              ? "flex items-center text-blue-600"
-              : "flex items-center text-gray-600"
+              ? "text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2"
+              : "text-gray hover:text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2"
           }
+          onClick={() => setActiveStep(previousStep())}
         >
-          <span className="flex items-center justify-center w-5 h-5 me-2 text-xs border border-gray-500 rounded-full shrink-0 dark:border-blue-500">
-            1
-          </span>
-          Upload file & Description
-          <svg
-            className="w-3 h-3 ms-2 sm:ms-4 rtl:rotate-180"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 12 10"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="m7 9 4-4-4-4M1 9l4-4-4-4"
-            />
-          </svg>
-        </li>
+          1. Upload file & Description
+        </button>
 
-        <li
+        <div className="pb-1.5">
+          <FaAnglesRight />
+        </div>
+
+        <button
+          type="button"
           className={
             activeStep === "second"
-              ? "flex items-center text-blue-600"
-              : "flex items-center text-gray-600"
+              ? "text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2"
+              : "text-gray hover:text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2"
           }
+          onClick={() => setActiveStep(nextStep())}
         >
-          <span className="flex items-center justify-center w-5 h-5 me-2 text-xs border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
-            2
-          </span>
-          User Agency & Summary
-        </li>
+          2. User Agency & Summary
+        </button>
       </ol>
       <div className="grid my-16 mx-50 gap-y-10">
         <StepperBody />
       </div>
-      <div className="flex justify-center">
-        <button
-          onClick={() => setActiveStep(previousStep())}
-          type="button"
-          className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-        >
-          Previous
-        </button>
-        <button
-          onClick={() => setActiveStep(nextStep())}
-          type="button"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Next
-        </button>
+
+      {/* Sticky footer */}
+      <div className="flex justify-center sticky bottom-0 bg-white rounded-lg shadow-2xl p-3">
+        {activeStep == "second" && (
+          <button
+            onClick={() => setActiveStep(previousStep())}
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            <div className="flex">
+              <div className="pr-1 pt-0.5">
+                <FaAnglesLeft />
+              </div>
+              Previous
+            </div>
+          </button>
+        )}
+
+        {activeStep == "first" && (
+          <button
+            onClick={() => setActiveStep(nextStep())}
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            <div className="flex">
+              Next
+              <div className="pl-1 pt-0.5">
+                <FaAnglesRight />
+              </div>
+            </div>
+          </button>
+        )}
       </div>
     </>
   );
